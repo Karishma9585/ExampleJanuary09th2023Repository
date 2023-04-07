ea7c113ac3f0bfdcdd79a6b1dac050370580c355
Karishma9585 <zainabb42509@gmail.com>
2023-04-07 09:13:03 +0530

    Intial Draft

---
+++ DemoProject/src/test/java/com/sgtesting/tests/users.java
@@ -0,0 +1,27 @@
+package com.sgtesting.tests;
+
+import org.testng.annotations.Test;
+
+public class users {
+	@Test 
+	public void method1()
+	{
+		System.out.println("It is a method1 Statement");
+	}
+	@Test 
+	public void method2()
+	{
+		System.out.println("It is a method2 Statement");
+	}
+	@Test 
+	public void method3()
+	{
+		System.out.println("It is a method3 Statement");
+	}
+	@Test 
+	public void method4()
+	{
+		System.out.println("It is a method4 Statement");
+	}
+
+}
